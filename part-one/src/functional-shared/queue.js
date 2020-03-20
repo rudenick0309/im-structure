e88eb96578f9f294829cc7f1f324d1e115ea8494
rudenick0309 <rudenick0309@gmail.com>
2020-03-20 11:59:36 +0900

    functional-shared_queue completed

--- part-one/src/functional-shared/queue.js
+++ part-one/src/functional-shared/queue.js
@@ -1,9 +1,39 @@
 const Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage : {},
+    front : 0,
+    rear : 0
+  };
+
+  extend(instance, queueMethods);
+  return instance;
 };
 
-const queueMethods = {};
+var extend = function(to, from) {
+  for(let key in from) {
+    to[key] = from[key];
+  }
+}
+
+var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+this.storage[this.rear] = value;
+this.rear += 1;
+};
+
+queueMethods.dequeue = function() {
+  if (this.rear === this.front) {
+    return ;
+  }
+  return this.storage[this.front++];
+};
+
+queueMethods.size = function() {
+return this.rear - this.front;
+};
 
 if (typeof module === "object" && typeof module.exports === "object") {
   module.exports = {
