cc61dda83d21449a11fc16dc169ca291694c8ee7
aiody <aiody329@gmail.com>
2020-03-20 16:59:34 +0900

    complete stack.js in prototypal

--- part-one/src/prototypal/stack.js
+++ part-one/src/prototypal/stack.js
@@ -1,9 +1,32 @@
 const Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.count = 0;
+  obj.storage = {};
+  return obj;
 };
 
 const stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count += 1;
+}
+
+stackMethods.pop = function() {
+  if(this.count < 1) {
+    return ;
+  }
+  var deleteObj = this.storage[this.count - 1];
+  delete this.storage[this.count - 1];
+  this.count -= 1;
+  return deleteObj;
+}
+
+stackMethods.size = function() {
+  return this.count;
+}
 
 module.exports = {
   Stack,
