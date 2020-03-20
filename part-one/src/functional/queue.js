4df131045dea059241860fd682d04851371b18f4
aiody <aiody329@gmail.com>
2020-03-20 10:45:50 +0900

    complete functional queue

--- part-one/src/functional/queue.js
+++ part-one/src/functional/queue.js
@@ -3,14 +3,29 @@ const Queue = function() {
 
   // Use an object with numeric keys to store values
   const storage = {};
-
+  var count = 0;
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {};
+  someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count += 1;
+  };
 
-  someInstance.dequeue = function() {};
+  someInstance.dequeue = function() {
+    if(count < 1) {
+      return {};
+    }
+    var deleteObj = storage[0];
+    for(let i = 0; i < count; i += 1) {
+      storage[i] = storage[i + 1];
+    }
+    count -= 1;
+    return deleteObj;
+  };
 
-  someInstance.size = function() {};
+  someInstance.size = function() {
+    return count;
+  };
 
   return someInstance;
 };
