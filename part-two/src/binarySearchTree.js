9531b3e23dbefd5fbd0a8b2e9ab8e9baf87728b0
aiody <aiody329@gmail.com>
2020-03-23 17:57:21 +0900

    complete tree.js and binarySearchTree.js

--- part-two/src/binarySearchTree.js
+++ part-two/src/binarySearchTree.js
@@ -1,4 +1,51 @@
-const BinarySearchTree = function(value) {};
+const BinarySearchTree = function(value) {
+  const binarySearchTree = {};
+  binarySearchTree.value = value;
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+
+  binarySearchTree.insert = function(value) {
+    if(binarySearchTree.value > value) {
+      if(binarySearchTree.left === null) {
+        binarySearchTree.left = new BinarySearchTree(value);
+      } else {
+        binarySearchTree.left.insert(value);
+      }
+    } else if(binarySearchTree.value < value) {
+      if(binarySearchTree.right === null) {
+        binarySearchTree.right = new BinarySearchTree(value);
+      } else {
+        binarySearchTree.right.insert(value);
+      }
+    }
+  };
+
+  binarySearchTree.contains = function(value) {
+    if(binarySearchTree.value === value) {
+      return true;
+    }
+    if(binarySearchTree.left !== null && binarySearchTree.left.contains(value)) {
+      return true;
+    }
+    if(binarySearchTree.right !== null && binarySearchTree.right.contains(value)) {
+      return true;
+    }
+    return false;
+  };
+
+  binarySearchTree.depthFirstLog = function(func) {
+    // pre-order
+    func(binarySearchTree.value);
+    if(binarySearchTree.left !== null) {
+      binarySearchTree.left.depthFirstLog(func);
+    }
+    if(binarySearchTree.right !== null) {
+      binarySearchTree.right.depthFirstLog(func);
+    }
+  };
+
+  return binarySearchTree;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- part-two/src/tree.js
+++ part-two/src/tree.js
@@ -1,18 +1,44 @@
+const extend = function(to, from) {
+  for(let key in from) {
+    to[key] = from[key];
+  }
+};
+
 const Tree = function(value) {
   const newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null; // fix me
+  newTree.children = [];
 
+  extend(newTree, treeMethods);
   return newTree;
 };
 
 const treeMethods = {};
 
-treeMethods.addChild = function(value) {};
+treeMethods.addChild = function(value) {
+  let tree = new Tree(value);
+  this.children.push(tree);
+};
 
-treeMethods.contains = function(target) {};
+treeMethods.contains = function(target) {
+  let findTarget = (target, newChildren) => {
+    for(let i = 0; i < newChildren.length; i++) {
+      if(newChildren[i].value === target) {
+        return true;
+      } else if(findTarget(target, newChildren[i].children)) {
+          return true;
+      }
+    }
+    return false;
+  };
+
+  if(this.value === target) {
+    return true;
+  } else {
+    return findTarget(target, this.children);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
